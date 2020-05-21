14facc1f4272dd253109f5187df485e2010133dd
Vyacheslav Popov <slavaavals616@gmail.com>
2020-05-22 00:01:48 +0400

    add sveltestrap

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
-	import UserList from './components/UserList.svelte'
+	import Table from './components/Table.svelte'
 	export let name;
 </script>
 
 <main>
 	Hello
-	<UserList />
+	<Table />
 </main>
---
+++ src/components/Table.svelte
@@ -0,0 +1,15 @@
+<script>
+import { Table } from 'sveltestrap'
+</script>
+
+<Table>
+  <thead>
+    <tr>
+      <td>№</td>
+      <td>Country</td>
+      <td>Cases</td>
+      <td>Deaths</td>
+      <td>Flag</td>
+    </tr>
+  </thead>
+</Table>
